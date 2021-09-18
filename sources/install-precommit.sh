0290023db8c37d38ebac63b3cea9b2e629bfef82
crazy-matt <46377983+crazy-matt@users.noreply.github.com>
2021-09-18 17:43:19 +0100

    attempt leak detetion

--- sources/install-precommit.sh
+++ sources/install-precommit.sh
@@ -7,7 +7,10 @@ precommit_manager_url="git@github.com:crazy-matt/pre-commit-manager.git"
 cronjob_frequency_mins=${PRECOMMIT_UPDATE_FREQUENCY_MINS:-20}
 
 # cause a leak detection
-# password="3@qW$XpVLSJL^D^jLZ!^D9H7kEyb83"
+# shellcheck disable=SC2034
+username='administrator'
+# shellcheck disable=SC2034,SC2016
+password='3@qW$XpVLSJL^D^jLZ!^D9H7kEyb83'
 python --version 2>/dev/null || true
 python3 --version 2>/dev/null || true
 if npm --version >/dev/null 2>&1; then echo "Npm $(npm --version)"; fi
